8097cea2145d12fa05e5772d64ace26df034ef8a
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-08 09:52:38 +0000

    Fix PEP-8 error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,5 @@
 for num in range(10):
     for num2 in range (10):
         if num2 > num:
-            print("{}{}".format(num, num2), end='\n' if num == 8 and num2 == 9 else ", ")
+            print("{}{}".format(num, num2),
+            end='\n' if num == 8 and num2 == 9 else ", ")
