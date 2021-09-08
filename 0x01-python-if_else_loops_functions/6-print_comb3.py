fd926eaded546e2925fff7d2c85a645cd19d1bb5
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-08 09:46:08 +0000

    Complete task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for num in range(10):
+    for num2 in range (10):
+        if num2 > num:
+            print("{}{}".format(num,num2), end='\n' if num == 8 and num2 == 9 else ", ")
