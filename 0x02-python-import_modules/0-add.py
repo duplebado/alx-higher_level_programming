7e44e20e426b4ad920d9e253030faf89b6beec43
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-14 08:48:23 +0000

    Add 'main' constraint

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-add = __import__('add_0').add
 
-a = 1
-b = 2
+if __name__ == '__main__':
+    add = __import__('add_0').add
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
