30937f6288311e39c6a28af36617507461e8b61e
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-14 10:09:17 +0000

    Fix PEP8 errrors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     from sys import argv
 
     print("{:d} {:s}{}".format(
-        len(argv) - 1, 
-        "arguments" if len(argv) != 2 else "argument", 
+        len(argv) - 1,
+        "arguments" if len(argv) != 2 else "argument",
         "." if len(argv) == 1 else ":"
         ))
 
