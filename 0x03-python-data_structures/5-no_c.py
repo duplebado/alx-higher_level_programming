c0e1bcf8ec5c32b00f1d853b121e62850d851fbd
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-14 22:19:08 +0000

    Complete task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    result = ""
+
+    for char in my_string:
+        if char != "C" and char != "c":
+            result += char
+
+    return result
