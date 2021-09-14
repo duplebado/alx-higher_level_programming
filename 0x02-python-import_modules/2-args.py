d5ded15c9f5e4008b5f071c10204dd2f9d63c822
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-14 09:35:00 +0000

    Complete task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    print("{:d} {:s}{}".format(len(argv) - 1, 
+        "arguments" if len(argv) != 2 else "argument", 
+        "." if len(argv) == 1 else ":"))
+
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
