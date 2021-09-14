1cc2fb7897031a3719d11703f2400828486e4614
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-14 07:36:56 +0000

    Complete task 14

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def alt_case_reverse_alpha():
+    for i in range(122, 96, -1):
+        if (not(i % 2)):
+            print(chr(i), end="")
+        else:
+            print(chr(i - 32), end="")
+
+alt_case_reverse_alpha()            
