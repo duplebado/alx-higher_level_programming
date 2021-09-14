413aa3b0d701f45380cc1964a7eb35e3047c62bc
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-14 21:58:29 +0000

    Complete task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-     if idx < 0 or idx >= len(my_list):
-         return my_list[:]
+    copy = my_list[:]
 
-     return my_list[idx:0]
+    if idx < 0 or idx >= len(my_list):
+        return copy
+
+    copy[idx:idx + 1] = [element]
+
+    return copy
