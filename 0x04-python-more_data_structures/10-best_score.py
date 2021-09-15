e41e25ecf3a72b17278209cadd815be7b507b1dc
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-15 21:55:24 +0000

    Return correct output

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,10 +5,12 @@ def best_score(a_dictionary):
     if a_dictionary == None or len(a_dictionary) == 0:
         return None
 
-    result = 0
+    current_high_score = 0
+    high_score_holder = None
 
     for key in a_dictionary:
-        if a_dictionary[key] > result:
-            result = a_dictionary[key]
+        if a_dictionary[key] > current_high_score:
+            current_high_score = a_dictionary[key]
+            high_score_holder =  key
 
-    return result
+    return high_score_holder
