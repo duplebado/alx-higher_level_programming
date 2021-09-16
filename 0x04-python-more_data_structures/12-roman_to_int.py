5ab4ae62aa93513b82aafd00889df770213a0e92
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-16 07:32:24 +0000

    Fix algo

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,6 @@ def roman_to_int(roman_string):
 
     r_nums = {
                 "I": 1, "V": 5,
-                "IV": 4, "IX": 9,
                 "X": 10, "L": 50,
                 "C": 100, "D": 500,
                 "M": 1000
@@ -17,17 +16,10 @@ def roman_to_int(roman_string):
     strLen = len(roman_string)
     i = 0
 
-    while i < strLen:
-        if roman_string[i] == "I" and i + 1 < strLen:
-            if roman_string[i + 1] == "V" or roman_string[i + 1] == "X":
-                makeshift_key = roman_string[i] + roman_string[i + 1]
-                result += r_nums[makeshift_key]
-                i += 2
-            else:
-                result += r_nums[roman_string[i]]
+    for i in range(strLen):
+        if i != strLen - 1 and r_nums[roman_string[i]] < r_nums[roman_string[i + 1]]:
+           result += r_nums[roman_string[i]] * -1
         else:
             result += r_nums[roman_string[i]]
-
-        i += 1
 
     return result
