2646b80f1134c99ef147f7e72eaf219b993adcfb
duplebado <ajibadeadebayoengr@gmail.com>
2021-09-16 08:33:49 +0000

    Fix algo

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,8 +2,13 @@
 
 
 def complex_delete(a_dictionary, value):
-    for key in a_dictionary:
+    def my_map(fn, a_list):
+        for list_item in a_list:
+            fn(list_item)
+
+    def del_key(key):
         if a_dictionary[key] == value:
             del a_dictionary[key]
 
+    my_map(del_key, list(a_dictionary.keys()))
     return a_dictionary
