0b827ec1c01f4cc52c93335c88a3dfe888a48862
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-05 11:34:20 +0000

    Fix algo

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,15 +5,15 @@ def safe_print_list_integers(my_list=[], x=0):
     i = 0
     actual_printed = 0
 
-    try:
-        while i < x:
+    while i < x:
+        try:
             print("{:d}".format(my_list[i]), end="")
             i += 1
             actual_printed += 1
+        except IndexError:
+            raise
+        except:
+            i += 1
 
-        print(end="\n")
-        return actual_printed
-    except IndexError:
-        raise;
-    except:
-        i += 1
+    print(end="\n")
+    return actual_printed
