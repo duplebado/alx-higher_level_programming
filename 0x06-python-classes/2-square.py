d8a699c272480e71d9d8cd52140d62d56ae816b3
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-05 18:54:01 +0000

    Complete task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Define a class Square"""
+"""Define a class Square."""
 
 
 class Square:
-    """Represent a square"""
+    """Represent a square."""
 
-    @__size.setter
-    def __size(self, size=0):
-        """ Pythonic setter """
-
-        if (type(size) is not int):
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
-        if (size < 0):
+        elif size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
