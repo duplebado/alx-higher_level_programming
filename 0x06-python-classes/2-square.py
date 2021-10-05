70b6ff7dbf89f748888e2be47834a5dddf8fcb04
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-05 17:58:13 +0000

    Attempt task 2 again

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,12 +5,15 @@
 class Square:
     """Represent a square"""
 
-    def __init__(self, size=0):
-        self.__size = size
-
-    @__size.setter
-    def __size(self, size=0):
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, size=0):
         if (type(size) is not int):
             raise TypeError("size must be an integer")
+
         if (size < 0):
             raise ValueError("size must be >= 0")
+
+        self.__size = size
