b76a0e34de003902d3eecaeda49d7f0129aea746
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-05 11:11:09 +0000

    Complete task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+
+    try:
+        while i < x:
+            print(my_list[i], end="")
+            i += 1
+
+        return i
+    except:
+        return i
+    finally:
+        print(end="\n")
