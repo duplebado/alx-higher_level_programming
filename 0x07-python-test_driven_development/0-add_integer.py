8d62f348dbf6c2758bc7af99b972cbfe09c87d3b
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-07 16:33:49 +0000

    Add documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,29 @@
 
 
 def add_integer(a, b=98):
+    """Computes the sum of two integers
+
+    Parameters
+    ----------
+        a: int, float 
+            The first number
+        b: int, float, optional
+            The second number
+
+    Raises
+    ------
+        TypeError
+            If either of a or b is a non-integer and non-float
+
+    Note
+    ----
+        Parameters/Arguments are typecasted to ints before addition is performed
+
+    Returns
+    -------
+        int
+            The sum of the two integers
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
 
