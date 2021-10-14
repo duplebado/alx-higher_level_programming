4f580ffcc645bfd3a60ff7a20714441d80c5ca59
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-14 14:25:28 +0000

    Complete task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -25,4 +25,4 @@ def inherits_from(obj, a_class):
                 true or false
 
     """
-    return isinstance(obj, a_class)
+    return issubclass(type(obj), a_class) and (type(obj) is not a_class)
