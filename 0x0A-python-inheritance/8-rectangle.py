a40b09632accceefc206fce664ae477ad99972c0
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-14 14:57:33 +0000

    Complete task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """ class BaseGeometry """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ Calculates area of base geometry
+            but not implemented
+
+            Raises
+            ------
+                Exception
+                    always
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+            Validates an integer
+
+            Parameters
+            ----------
+                name : str
+                    name of the integer to be validated
+
+                value: int
+                    integer to be validated
+
+            Raises
+            ------
+                TypeError
+                    if @value is not an integer
+
+                ValueError
+                    if @value is less than or equal to zero
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
