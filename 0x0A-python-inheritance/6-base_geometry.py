deeba447ff8ab9dc9d5a51ce417765fc25c3477f
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-14 14:33:35 +0000

    Complete task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """ class BaseGeometry """
+
+    def area(self):
+        """ Calculates area of base geometry
+            but not implemented
+
+            Raises
+            ------
+                Exception
+                    always
+        """
+        raise Exception("area() is not implemented")
