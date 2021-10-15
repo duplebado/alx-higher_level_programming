133241bee51d9e5b7f94f0b11457f9110dbea904
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-15 09:01:21 +0000

    Complete task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,24 @@
     and then save them to a file
 """
 from sys import arg
-
-
-if __name__ == "__main__":
-    save_to_json_file, load_from_json_file = (
+save_to_json_file, load_from_json_file = (
             __import__('5-save_to_json_file').save_to_json_file,
             __import__('6-load_from_json_file').load_from_json_file
             )
 
-    items = load_from_json_file("add_item.json")
+
+def run_main():
+    """ Aadds all arguments to a Python list, and
+        then save them to a file
+    """
+    try:
+        items = load_from_json_file("add_item.json")
+    except:
+        items = []
 
     items += arg[1:]
 
     save_to_json_file(items, "add_item.json")
+
+if __name__ == "__main__":
+    run_main()
