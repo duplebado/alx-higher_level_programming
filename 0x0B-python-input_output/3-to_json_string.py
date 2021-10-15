2b72b622de952c40f72c0654932a92f5414f0730
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-15 07:47:47 +0000

    Complete task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" Defines a function that returns the JSON representation of an object (string)"""
+""" Defines a function that returns the JSON representation
+    of an object (string)
+"""
 import json
+
 
 def to_json_string(my_obj):
     """ Return JSON representation of an object
-        
+
         Parameter
         ---------
             my_obj : Object
