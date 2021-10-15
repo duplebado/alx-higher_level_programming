83b23b9816255789e8465bbc191f4595d7fe551f
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-15 09:09:28 +0000

    Complete task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Add all arguments to a Python list and save them to a file."""
+
+
 from sys import arg
 save_to_json_file, load_from_json_file = (
             __import__('5-save_to_json_file').save_to_json_file,
