4954e8395ec77b7a1428da4c8670a736a81fa401
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-16 02:13:46 +0000

    Complete task 14

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -294,7 +294,7 @@ class Rectangle(Base):
         return {
                 "id": self.id,
                 "width": self.width,
-                "height": self.height.
+                "height": self.height,
                 "x": self.x,
                 "y": self.y
             }
