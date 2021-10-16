782273d55b5eb09624841a033e8f2a4054a29b10
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-16 01:30:07 +0000

    Complete task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Defines a Square class"""
+Rectangle = __import("rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initialize an instance of Square
+
+            Parameters
+            ----------
+                size :
+                x :
+                y :
+                id :
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ String representation of the Square
+            instance
+
+            Returns
+            -------
+                str
+                    the Square instance representation
+        """
+
+        return "[Square] ({}) {}/{} - {}".format(
+                    self.id, self.__x, self.__y, self.__width
+                )
