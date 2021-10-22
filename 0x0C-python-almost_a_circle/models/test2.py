6dc1758ac21a1be04cc6aa9165fa3eb7507912da
duplebado <ajibadeadebayoengr@gmail.com>
2021-10-22 20:30:39 +0000

    Add tests

---
+++ 0x0C-python-almost_a_circle/models/test2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import tkinter as tk
+#import Tkinter as tk  # for python 2
+    
+def create_window():
+    window = tk.Toplevel(root)
+
+root = tk.Tk()
+b = tk.Button(root, text="Create new window", command=create_window)
+b.pack()
+
+root.mainloop()
