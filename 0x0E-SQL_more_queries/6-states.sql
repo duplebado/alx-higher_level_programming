8d9b8316dc21e12b26bd4a48cd64b70a221f39d4
duplebado <ajibadeadebayoengr@gmail.com>
2022-01-05 08:23:16 +0000

    Complete task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- creates the the database "hbtn_0d_usa" and the table "states"
+-- - "states" description:
+--   	id INT unique, auto generated, can’t be null and is a primary key
+--   	name VARCHAR(256) can’t be null
+-- - If the database "hbtn_0d_usa" already exists, your script should not fail
+-- - If the table "states" already exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS states (
+	`id` INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`name` VARCHAR(256) NOT NULL
+	);
