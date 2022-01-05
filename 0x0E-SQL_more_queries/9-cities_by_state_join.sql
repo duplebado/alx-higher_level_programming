7b4c4929f46fc5213fe96c27f22bbb4ed285a249
duplebado <ajibadeadebayoengr@gmail.com>
2022-01-05 09:22:36 +0000

    Attempt

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- lists all cities contained in the database
 
-SELECT * FROM cities JOIN states ON cities.state_id=states.id ORDER BY id;
+SELECT * FROM cities JOIN states ON cities.state_id=states.id ORDER BY cities.id;
