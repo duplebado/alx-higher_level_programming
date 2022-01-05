f98cc3765afd99cc8c54f659addb77aa13735a3c
duplebado <ajibadeadebayoengr@gmail.com>
2022-01-05 08:38:25 +0000

    Debug

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -13,5 +13,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	`id` INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` VARCHAR(256) NOT NULL,
-	`state_id` INT NOT NULL FOREIGN KEY REFERENCES states(id)
-	);
+	`state_id` INT NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
