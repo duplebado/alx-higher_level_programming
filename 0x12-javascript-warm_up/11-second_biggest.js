8449cd98aea4cfda3a9603b18863e2b607550129
duplebado <ajibadeadebayoengr@gmail.com>
2022-01-09 00:58:36 +0000

    Fix semistandard validation error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,25 +3,25 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let big1 =  Number(process.argv[2]);
+  let big1 = Number(process.argv[2]);
   let big2 = Number(process.argv[3]);
 
   for (let i = 4; i < process.argv.length; i++) {
     let potentialBig = Number(process.argv[i]);
     let temp;
-    
-    if (big1 < potentialBig){
+
+    if (big1 < potentialBig) {
       temp = big1;
       big1 = potentialBig;
       potentialBig = temp;
     }
 
-    if (big2 < potentialBig){
+    if (big2 < potentialBig) {
       temp = big2;
       big2 = potentialBig;
       potentialBig = temp;
     }
   }
 
-  console.log(big1 < big2 ? big1 : big2)
+  console.log(big1 < big2 ? big1 : big2);
 }
