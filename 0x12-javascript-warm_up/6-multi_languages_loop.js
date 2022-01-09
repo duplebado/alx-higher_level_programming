6976d3aa05f9c8a160488d1b798d7fcfc7d2eea6
duplebado <ajibadeadebayoengr@gmail.com>
2022-01-09 00:18:51 +0000

    Fix semistandard validation error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < lines.length; i++){
-	console.log(lines[i]);
-};
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
